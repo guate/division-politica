d599d65a7492dd4a522882dd3d3d03d8f674bbd9
Darwin Monroy <contact@darwinmonroy.com>
2016-03-26 15:17:13 -0600

    remove bumpversion, enable setuptools_scm

--- guate/division_politica/__init__.py
+++ guate/division_politica/__init__.py
@@ -1,2 +1,1 @@
 __author__ = 'Darwin Monroy'
-__version__ = '0.1.1'
--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 #-*- coding: utf-8 -*-
-from distutils.core import setup
-from guate import division_politica
+from setuptools import setup
 
 with open('README.md', 'r') as ld:
     long_description = ld.read()
 
 setup_args = dict(
     name='guate.division-politica',
-    version=division_politica.__version__,
+    use_scm_version=True,
     packages=['guate.division_politica'],
     include_package_data=True,
     author='Darwin Monroy',
     author_email='contact@darwinmonroy.com',
     description='División Política de Guatemala',
-    long_description=long_description
-
+    long_description=long_description,
+    setup_requires=[
+        'setuptools_scm',
+    ],
 )
 
 
